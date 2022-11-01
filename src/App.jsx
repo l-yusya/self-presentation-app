f11fc5a352809ab003ae6ccbacbe3729b0da05c9
l-yusya <105113818+l-yusya@users.noreply.github.com>
2022-11-01 13:56:39 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,65 @@
+import "./App.css";
+
+function App() {
+  const user = {
+    name: "Yuliia",
+    workingExperience: {
+      "English teacher": 9,
+      "ELT trainer": 4,
+    },
+    hobbies: ["Singing", "Embroidery", "Bindge-watching TV series"],
+    project: "shchedryk.com.ua",
+  };
+
+  const websiteLink = "https://" + user.project;
+  return (
+    <main className="app">
+      <div className="intro">
+        <h1 className="title">About me</h1>
+
+        <p className="text">Get to know me a bit!</p>
+        <p className="text">My name is {user.name}, nice to meet you!</p>
+        <img className="photo" src="./src/photo.jpg"></img>
+        <p className="text">Fun facts about me:</p>
+        <ul>
+          <li className="list-item">
+            I am an English teacher! I have been teaching for{" "}
+            {user.workingExperience["English teacher"]} years now.
+          </li>
+          <li className="list-item">
+            For the last {user.workingExperience["ELT trainer"]} years I have
+            been working as an ELT trainer.
+          </li>
+          <li className="list-item">
+            My hobbies are:
+            <div>
+                <ul className="hobbies">
+                  {user.hobbies.map((hobby) => (
+                    <li key={hobby}>{hobby}</li>
+                  ))}
+                </ul>
+                </div>
+              <div className="hobbies-img">
+                <img className="hobby-img" src="./src/singing.jpeg"></img>
+                <img className="hobby-img" src="./src/embroidery.jpg"></img>
+                <img
+                  className="hobby-img"
+                  src="./src/bindge-watching.jpg"
+                ></img>
+              </div>
+          </li>
+          <li className="list-item">
+            I decided to switch to front-end when the war started, although I
+            used to create websites before, at school.
+          </li>
+          <li className="list-item">
+            You can see my first commercial project{" "}
+            <a href={websiteLink}>here</a>. It's my choir's website!
+          </li>
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+export default App;
